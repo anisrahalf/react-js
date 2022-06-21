2b836d30c4174111f53ad4ed706d7901e46404e4
Anis Rahal Filho <anisrahalf@gmail.com>
2022-06-21 20:11:24 -0300

    :sparkles: Add the scoreboard into the main.js file

--- main.js
+++ main.js
@@ -3,15 +3,13 @@ import "./src/components/styles/settings/generics/reset.css"
 import "./src/components/styles/settings/elements/base.css"
 
 import BoardGame from "./src/components/objects/boardgame";
-import PlayerName from "./src/components/PlayerName";
+import Scoreboard from "./src/components/objects/boardgame/ScoreBoard";
 
 const $root = document.querySelector("#root");
 
 $root.insertAdjacentHTML(
     "beforeend",
-    `
-    ${PlayerName('Player1')} 
-    ${PlayerName('Player2')}
+    `${Scoreboard()}
     ${BoardGame(6)}
     `
 )
