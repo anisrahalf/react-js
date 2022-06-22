627e489a551abbda2a24d3618843cf1b3986d80f
Anis Rahal Filho <anisrahalf@gmail.com>
2022-06-22 18:23:30 -0300

    :construction: Create the VsPlayer component without CSS

--- src/components/objects/boardgame/ScoreBoard/index.js
+++ src/components/objects/boardgame/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import PlayerName from "../../../PlayerName";
+import VsPlayer from "../../../VsPlayer";
 import "./style.css";
 
 function Scoreboard() {
     return /*html*/ `
     <header class ="score-board">
     ${PlayerName('Player1')} 
+    ${VsPlayer()}
     ${PlayerName('Player2')}
     </header>
     `
