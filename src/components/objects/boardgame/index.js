e769e5060bfc1da26c140232caa3c367fdadb46d
Anis Rahal Filho <anisrahalf@gmail.com>
2022-06-23 11:30:35 -0300

    :bug: Swap player when 2 cards flip

--- src/components/objects/boardgame/index.js
+++ src/components/objects/boardgame/index.js
@@ -4,12 +4,36 @@ import cards from "./data";
 
 function BoardGame(amountCards) {
 
+    const flipAndhideCards = ($cardsActive) => {
+        $cardsActive.forEach((card) => card.classList.remove('-active'))
+    }
+    const changePlayer = () => {
+        const $arrowDown = document.querySelector('.arrow-down')
+        const currentplayer = $arrowDown.getAttribute('data-currentplayer')
+        $arrowDown.setAttribute('data-currentplayer', currentplayer == 1 ? 2 : 1)
+    }
+
+    window.boardGame = {};
+    window.boardGame.handleClick = () => {
+        const $boardGame = document.querySelector('.board-game')
+
+        const $cardsActive = $boardGame.querySelectorAll('.card-front-back.-active')
+
+        if ($cardsActive.length == 2) {
+            setTimeout (() => {
+
+                flipAndhideCards($cardsActive)
+                changePlayer()
+            }, 1000)
+
+        }
+    }
     const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon));
     const $htmlCards = htmlCardsList.join('');
    
 
     return /*html*/`
-    <section class="board-game">
+    <section class="board-game" onClick="boardGame.handleClick()">
         ${$htmlCards}
     </section>
     `
